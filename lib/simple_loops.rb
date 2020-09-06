c248baba8dfaef83e6d1b97b84c9431d042f30b6
Thomas Bigelow <tjbigelow@att.net>
2020-09-06 20:43:47 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -4,3 +4,17 @@ def loop_message_five_times(message)
     puts message
   end
 end
+
+def loop_message_n_times(message, n)
+  n.times do
+    puts message
+  end
+end
+
+def output_array(array)
+  i = 0
+  while i < array.length do
+    puts array[i]
+    i++
+  end
+end
